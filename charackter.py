8db22c73ad68ad5d8cf7c06753d2425dcf99cc77
NazarGniluh <shuzocode@gmail.com>
2023-12-13 17:27:09 +0200

    part5

--- charackter.py
+++ charackter.py
@@ -3,28 +3,40 @@ class Charackter:
     def __init__(self , x , y , w ,h ,speed , texture):
         self.speed = speed
         self.texture = pygame.image.load(texture)
+        self.texture = pygame.transform.scale(self.texture, (w, h))
         self.hitbox = self.texture.get_rect()
         self.hitbox.x = x
         self.hitbox.y = y
-        self.texture = pygame.transform.scale(self.texture , (w , h))
+        self.sound = pygame.mixer.Sound('byistraya-hodba-po-betonnoy-poverhnosti-26128.mp3')
+        self.sound.play(-1)
+        self.sound.set_volume(0)
 
     def render(self , window):
         window.blit(self.texture , (self.hitbox.x , self.hitbox.y))
 
     def move(self):
         keys = pygame.key.get_pressed()
+        step = False
         if keys[pygame.K_d]:
             if self.hitbox.x < 750 :
                 self.hitbox.x += self.speed
+                step = True
         if keys[pygame.K_a]:
             if self.hitbox.x > 0:
                 self.hitbox.x -= self.speed
+                step = True
         if keys[pygame.K_w]:
             if self.hitbox.y > 0:
                 self.hitbox.y -= self.speed
+                step = True
         if keys[pygame.K_s]:
             if self.hitbox.y < 450:
                 self.hitbox.y += self.speed
+                step = True
+        if step == True :
+            self.sound.set_volume(0.5)
+        else:
+            self.sound.set_volume(0)
 
 
 
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import eneny
 import Gold
 from wall import Wall
 import random
+import time
 
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load('the bab baby of getto.mp3')
-pygame.mixer.music.play(-1)
+#pygame.mixer.music.load('the bab baby of getto.mp3')
+#pygame.mixer.music.play(-1)
+moneysound = pygame.mixer.Sound('money.ogg')
 
 window = pygame.display.set_mode((800 , 500))
 fps = pygame.time.Clock()
@@ -18,17 +20,24 @@ fon =  pygame.transform.scale(fon  , (800 , 500))
 
 walls = []
 
+
 spidor = charackter.Charackter(250 , 350 , 50 , 50  ,5  , "pixelartbobr2.png")
-enemy = eneny.Enemy(130 , 340 , 50 , 50  ,2  , "pixil-frame-0.png" , 100 ,200 ,300 ,300)
-gold1 = Gold.Gold(400 , 250 , 50 , 50 , "pixelartbottle.png")
-
-walls.append(Wall(random.randint(-200 , 600) , random.randint(0 , 490) , 200 , 10 ,(0 , 0 , 0)))
-walls.append(Wall(random.randint(0 , 790) , random.randint(0 , 300) , 10 , 200 ,(255 , 255 , 255)))
-walls.append(Wall(random.randint(-200 , 600) , random.randint(0 , 490) , 200 , 10 ,(0 , 0 , 0)))
-walls.append(Wall(random.randint(0 , 790) , random.randint(0 , 300) , 10 , 200 ,(255 , 255 , 255)))
+enemy = eneny.Enemy(300 , 150 , 50 , 50  ,2  , "pixil-frame-0.png" , 300 ,150 ,550 ,350)
+gold1 = Gold.Gold(400 , 400 , 50 , 50 , "pixelartbottle.png")
 
 
+walls.append(Wall( 100 , 100 , 200 , 10 ,(0 , 0 , 0)))
+walls.append(Wall( 100 , 100 , 10 , 200 ,(255 , 255 , 255)))
+walls.append(Wall( 100 , 300 , 200 , 10 ,(0 , 0 , 0)))
+walls.append(Wall( 300 , 300 , 10 , 200 ,(255 , 255 , 255)))
+walls.append(Wall( 430 , 100 , 200 , 10 ,(0 , 0 , 0)))
+walls.append(Wall( 600 , 100 , 10 , 200 ,(255 , 255 , 255)))
+walls.append(Wall( 500 , 300 , 10 , 200 ,(255 , 255 , 255)))
+walls.append(Wall( 300 , 200 , 200 , 10 ,(0 , 0 , 0)))
+walls.append(Wall( 250 , 300 , 200 , 10 ,(0 , 0 , 0)))
 
+
+sttime  = time.time()
 game = True
 while game :
     for event in pygame.event.get():
@@ -39,11 +48,28 @@ while game :
             game = False
             pygame.quit()
 
+
+    for wall in walls:
+        if spidor.hitbox.colliderect(wall.rect):
+            game = False
+            pygame.quit()
+
+    if spidor.hitbox.colliderect(gold1.hitbox):
+        moneysound.play()
+        gold1.hitbox.x  = 10000
+
     spidor.move()
     enemy.move()
 
     window.fill((255 , 0 , 0))
     window.blit(fon ,  (0  , 0 ))
+
+    if time.time() - sttime > 1:
+        sttime = time.time()
+        for u in walls:
+            u.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
     for i in walls:
         i.render(window)
     gold1.render(window)
